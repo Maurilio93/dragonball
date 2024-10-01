4d836bdc65bb4c0c4b45df398d25acd00c47190b
Maurilio Valenti <maurilio.valenti@gmail.com>
2024-10-01 16:31:25 +0200

    added changes

--- src/components/pagine/Home.jsx
+++ src/components/pagine/Home.jsx
@@ -1,39 +1,6 @@
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Button } from '../globali/Button';
 
-// Button Component
-export function Button({ type, text, onClick }) {
-  return (
-    <button
-      className="
-          bg-green-600
-          rounded-lg
-          px-5
-          py-2.5
-          text-white
-          text-lg
-          hover: border-green-600
-          hover: border-2
-          cursor-pointer
-          hover:bg-white
-          hover:text-black
-          focus:outline-none
-        "
-      type={type}
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  );
-}
-
-Button.propTypes = {
-  type: PropTypes.string.isRequired,
-  text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  onClick: PropTypes.func,
-};
-
-// Home Component
 export function Home() {
   return (
     <header className="bg-white">
